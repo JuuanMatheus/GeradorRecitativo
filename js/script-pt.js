1563becab151605fb740e9ef8a97af5d532fb0f0
Juan Matheus Viana Brito <juanjd10@live.com>
2026-06-23 21:52:00 -0300

    Organizando o projeto

--- js/script-pt.js
+++ js/script-pt.js
@@ -156,9 +156,9 @@ function baixarImagem() {
 }
 
 function paginaPortugues() {
-    window.location.href = "./index.html";
+    window.location.href = "index.html";
 }
 
 function paginaEspanhol() {
-    window.location.href = "./es/index.html";
+    window.location.href = "es/index.html";
 }
